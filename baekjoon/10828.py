aa894071a0015362cf7885815aa0e399b9fef98a
passionhuman <dlawnsghks12@naver.com>
2023-01-29 16:51:09 +0900

    10828(2)

--- baekjoon/10828.py
+++ baekjoon/10828.py
@@ -1,12 +1,11 @@
 import sys
 
 
-stack = []
+stack = [] # 리스트라는 스택생성
 N = int(input())
 
-
-for i in range(N):
-    command = list(map(str, sys.stdin.readline().rstrip().split()))
+for i in range(N): # 입력받은 N번의 횟수 돌고
+    command = list(map(str, sys.stdin.readline().rstrip().split())) # 입력을 여러번 받으므로 Sys 사용하여 시간초과 안 나게
     if command[0] == "push":
         stack.append(command[1])
     elif command[0] == "pop":
